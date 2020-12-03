ceda3fc69afeb63010fb5dad31c8bee2985be81d
Mario Manuel Mabande <54041357+mariomthree@users.noreply.github.com>
2020-12-03 17:15:04 +0200

    adjust exercise number 10

--- pratice/exercise_10.py
+++ pratice/exercise_10.py
@@ -1,17 +1,16 @@
-# Python Program to find Area of a Rectangle
+width = height = -1
 
-width = float(input('Please Enter the Width of a Rectangle: '))
-if width < 0 :
-    print("\033[31mPlease enter a positive number for width.\033[m")
+while width < 0:  
+    width = float(input('Please Enter the Width of a Rectangle: '))
+    if width < 0 :
+        print("\033[31mPlease enter a positive number for width.\033[m")
 
-height = float(input('Please Enter the Height of a Rectangle: '))
-if height < 0:
-    print("\033[31mPlease enter a positive number for height.\033[m")
+while height < 0:
+    height = float(input('Please Enter the Height of a Rectangle: '))
+    if height < 0:
+        print("\033[31mPlease enter a positive number for height.\033[m")
 
-if height >= 0 and width >= 0:
-    # calculate the area
-    Area = width * height
-    # calculate the Perimeter
-    Perimeter = 2 * (width + height)
-    print("\nArea of a Rectangle is: %.2f" %Area)
-    print("Perimeter of Rectangle is: %.2f" %Perimeter)
+Area = width * height
+Perimeter = 2 * (width + height)
+print("\nArea of a Rectangle is: %.2f" %Area)
+print("Perimeter of Rectangle is: %.2f" %Perimeter)
