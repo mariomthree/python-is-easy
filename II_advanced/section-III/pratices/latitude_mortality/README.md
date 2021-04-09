53d7b488c8e6ed546d4ab3a64d1cee25e84015a0
Mario Manuel Mabande <mario99manuel@gmail.com>
2021-04-09 12:22:53 +0200

    create readme

---
+++ II_advanced/section-III/pratices/latitude_mortality/README.md
@@ -0,0 +1,39 @@
+Practical Exercise
+
+## Prerequires
+For these practical exercises, you must install the libraries below.
+- <a target="_blank" href="https://pypi.org/project/pandas/">pandas</a>
+- <a target="_blank" href="https://pypi.org/project/numpy/">numpy</a>
+- <a target="_blank" href="https://pypi.org/project/seaborn/">seaborn</a>
+- <a target="_blank" href="https://pypi.org/project/matplotlib/">matplotlib</a>
+- <a target="_blank" href="https://pypi.org/project/statsmodels/">statsmodels</a>
+- <a target="_blank" href="https://pypi.org/project/scipy/">scipy</a>
+
+Resolution Imagens
+1.  Load the data and print the column names
+<img src="img/img_1.png">
+
+2.  Generate descriptive statistics for the data
+<img src="img/img_2.png">
+
+3.  Create a line plot for the variables. Add a title and x & y axes.
+    - Beautify the x-labels
+    - plot a line graph
+
+    <img src="img/img_3.png">
+
+4.  Create a scatter plot / Add title and (X,Y) axis names
+<img src="img/img_4.png">
+
+5.  Create a boxplot for mortality
+<img src="img/img_5.png">
+
+6.  Conduct a Pearson’s correlation test for the variables
+<img src="img/img_6.png">
+
+7.  Create a pair plot for the data
+<img src="img/img_7.png">
+
+9.  Create a Seaborn regplot of the regression model and a 95% confidence interval
+#plot the regression model y ~ x and a 95% confidence interval for that 
+<img src="img/img_9.png">
--- II_advanced/section-III/pratices/latitude_mortality/main.py
+++ II_advanced/section-III/pratices/latitude_mortality/main.py
@@ -1,12 +1,10 @@
 import pandas as pd
-from pandas import DataFrame
 import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
-from statsmodels.formula.api import ols
-import statsmodels.api as sm
-import os
+import matplotlib.pyplot as plt
+import statsmodels.formula.api as smf
 from scipy.stats import pearsonr
+import os
 
 def getCurrentDirname():
     dirname, filename = os.path.split(os.path.abspath(__file__))
@@ -14,56 +12,56 @@ def getCurrentDirname():
 
 #1: Load the data and print the column names
 recordsDataFrame = pd.read_csv(getCurrentDirname()+"/lmdata.csv")
-# print(recordsDataFrame)
-# print(recordsDataFrame.columns)
+print(f"Records:\n{recordsDataFrame} \n")
+print(f"Columns:\n{recordsDataFrame.columns}")
 
 #2: Generate descriptive statistics for the data
-# print(recordsDataFrame.describe())
+print(recordsDataFrame.describe())
 
-#3: Create a line plot for the variables. Add a title and x & y axes.
-# a) Beautify the x-labels
-# b) plot a line graph
+# 3: Create a line plot for the variables. Add a title and x & y axes.
+#   a) Beautify the x-labels
+#   b) plot a line graph
+ypoints = recordsDataFrame['latitude'].to_numpy()
+xpoints = recordsDataFrame['mortality'].to_numpy()
 
+plt.xlabel("Mortality")
+plt.plot(xpoints, ypoints,  marker = '.')
+plt.show()
 
-# ypoints = recordsDataFrame['latitude'].to_numpy()
-# xpoints = recordsDataFrame['mortality'].to_numpy()
+# 4: Create a scatter plot / Add title and (X,Y) axis names
+ypoints = recordsDataFrame['latitude'].to_numpy()
+xpoints = recordsDataFrame['mortality'].to_numpy()
 
-# plt.xlabel("Mortality")
-# plt.plot(xpoints, ypoints,  marker = '.')
+plt.title("Statistics")
+plt.xlabel("Mortality")
+plt.ylabel("Latitude")
+plt.scatter(xpoints, ypoints,  marker = '.')
+plt.show()
 
-# plt.show()
+# 5: Create a boxplot for mortality
+plt.boxplot(x="mortality",data=recordsDataFrame)
+plt.title("Mortality")
+plt.show()
 
-#4: Create a scatter plot / Add title and (X,Y) axis names
-
-# ypoints = recordsDataFrame['latitude'].to_numpy()
-# xpoints = recordsDataFrame['mortality'].to_numpy()
-
-# plt.title("Statistics")
-# plt.xlabel("Mortality")
-# plt.ylabel("Latitude")
-# plt.scatter(xpoints, ypoints,  marker = '.')
-
-# plt.show()
-
-#5: Create a boxplot for mortality
-
-# plt.boxplot(x="mortality",data=recordsDataFrame)
-# plt.title("Mortality")
-# plt.show()
-
-#6: Conduct a Pearson’s correlation test for the variables
+# 6: Conduct a Pearson’s correlation test for the variables
 latitude = recordsDataFrame['latitude'].to_numpy()
 mortality = recordsDataFrame['mortality'].to_numpy()
 
 correlation = pearsonr(mortality, latitude)
-#correlation2 = np.corrcoef(mortality, latitude)
-# print(f"Pearson’s correlation {correlation}")
+print(f"Pearson’s correlation: {correlation}")
 
-#7: Create a pair plot for the data
-# sns.pairplot(recordsDataFrame)
-# plt.show()
-
-#09: Create a Seaborn regplot of the regression model and a 95% confidence interval
-#plot the regression model y ~ x and a 95% confidence interval for that regression
-sns.regplot(x="mortality", y="latitude", data=recordsDataFrame, scatter_kws={"s": 80})
+# 7: Create a pair plot for the data
+sns.pairplot(recordsDataFrame)
 plt.show()
+
+# 09: Create a Seaborn regplot of the regression model and a 95% confidence interval
+# plot the regression model y ~ x and a 95% confidence interval for that regression
+sns.regplot(x="mortality", y="latitude",data=recordsDataFrame, ci=95, marker=".")
+plt.show()
+
+# 8: Type in this command: from statsmodels.formula.api import ols
+# 10: Create the regression model using ols() from the statsmodel package
+# 11: Print the model Summary
+results = smf.ols(formula='mortality ~ latitude', data=recordsDataFrame)
+results = results.fit()
+print(results.summary())
